fa413dd4816f07143ef5ba54a6c607e5d6defbe2
nigaels <cheukanigel@gmail.com>
2025-10-23 11:16:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Scroll to Top Button
+let mybutton = document.getElementById("scrollBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
